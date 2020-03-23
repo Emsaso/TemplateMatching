131faec79f47249fb0ad2b0743d5194d792881b9
Emsaso <34371682+Emsaso@users.noreply.github.com>
2020-03-23 13:45:49 +0100

    Initial commit

---
+++ TemplateMatching/MainForm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Accord;
+using Accord.Imaging;
+using System.Drawing.Imaging;
+using Accord.Imaging.Filters;
+
+namespace TemplateMatching
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.Filter = "Jpeg|*.jpg";
+                DialogResult dr = openFileDialog1.ShowDialog();
+                if (dr == DialogResult.OK)
+                {
+                    textBox1.Text = openFileDialog1.FileName;
+                    PictureInput.Load(openFileDialog1.FileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception:" + ex.ToString());
+            }
+        }
+
+        private void btnMatch_Click(object sender, EventArgs e)
+        {
+            //Converting Template into GrayScale Image            
+            Bitmap templateImage = new Bitmap(textBox2.Text);
+            Grayscale gg = new GrayscaleBT709();
+            Bitmap grayTemplate = gg.Apply(templateImage);
+
+            // create template matching algorithm's instance
+            ExhaustiveTemplateMatching tm = new ExhaustiveTemplateMatching(0);
+            // find all matchings with specified above similarity
+            Bitmap sourceImage = new Bitmap(textBox1.Text);
+            Bitmap graySource = gg.Apply(sourceImage);
+            
+            TemplateMatch[] matchings = tm.ProcessImage(graySource, grayTemplate);
+            
+            Graphics g = Graphics.FromImage(sourceImage);
+            if (matchings[0].Similarity > 0.8f)
+            {
+                int X = matchings[0].Rectangle.X;
+                int Y = matchings[0].Rectangle.Y;
+
+                g.DrawRectangle(new Pen(Color.Red, 3), X, Y, matchings[0].Rectangle.Width, matchings[0].Rectangle.Height);                
+                PicTemplate.Image = sourceImage;
+                MessageBox.Show("Match found...");
+            }
+            else
+            {
+                MessageBox.Show("Match Not Found...");
+            }           
+        }
+
+        private void btnTemplate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.Filter = "Jpeg|*.jpg";
+                DialogResult dr = openFileDialog1.ShowDialog();
+                if (dr == DialogResult.OK)
+                {
+                    textBox2.Text = openFileDialog1.FileName;
+                    //PictureInput.Load(openFileDialog1.FileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception:" + ex.ToString());
+            }
+        }
+    }
+}
+    
---
+++ TemplateMatching/MainForm.designer.cs
@@ -0,0 +1,186 @@
+﻿namespace TemplateMatching
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnMatch = new System.Windows.Forms.Button();
+            this.PicTemplate = new System.Windows.Forms.PictureBox();
+            this.PictureInput = new System.Windows.Forms.PictureBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnTemplate = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PicTemplate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox1.Controls.Add(this.btnTemplate);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnMatch);
+            this.groupBox1.Controls.Add(this.PicTemplate);
+            this.groupBox1.Controls.Add(this.PictureInput);
+            this.groupBox1.Controls.Add(this.btnOpen);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(566, 381);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Template Matching";
+            // 
+            // btnMatch
+            // 
+            this.btnMatch.Location = new System.Drawing.Point(481, 81);
+            this.btnMatch.Name = "btnMatch";
+            this.btnMatch.Size = new System.Drawing.Size(59, 24);
+            this.btnMatch.TabIndex = 5;
+            this.btnMatch.Text = "Match";
+            this.btnMatch.UseVisualStyleBackColor = true;
+            this.btnMatch.Click += new System.EventHandler(this.btnMatch_Click);
+            // 
+            // PicTemplate
+            // 
+            this.PicTemplate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PicTemplate.Location = new System.Drawing.Point(305, 120);
+            this.PicTemplate.Name = "PicTemplate";
+            this.PicTemplate.Size = new System.Drawing.Size(235, 223);
+            this.PicTemplate.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PicTemplate.TabIndex = 4;
+            this.PicTemplate.TabStop = false;
+            // 
+            // PictureInput
+            // 
+            this.PictureInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PictureInput.Location = new System.Drawing.Point(32, 120);
+            this.PictureInput.Name = "PictureInput";
+            this.PictureInput.Size = new System.Drawing.Size(235, 223);
+            this.PictureInput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureInput.TabIndex = 3;
+            this.PictureInput.TabStop = false;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(447, 44);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(28, 24);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "...";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(146, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(295, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Main Image:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(146, 82);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(295, 25);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Zoomed Image:";
+            // 
+            // btnTemplate
+            // 
+            this.btnTemplate.Location = new System.Drawing.Point(447, 81);
+            this.btnTemplate.Name = "btnTemplate";
+            this.btnTemplate.Size = new System.Drawing.Size(28, 24);
+            this.btnTemplate.TabIndex = 8;
+            this.btnTemplate.Text = "...";
+            this.btnTemplate.UseVisualStyleBackColor = true;
+            this.btnTemplate.Click += new System.EventHandler(this.btnTemplate_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(590, 405);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MainForm";
+            this.Text = "Template Matching";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PicTemplate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureInput)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox PicTemplate;
+        private System.Windows.Forms.PictureBox PictureInput;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnMatch;
+        private System.Windows.Forms.Button btnTemplate;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
